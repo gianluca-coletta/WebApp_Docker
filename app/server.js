10e946f1956ccebdd60b73ded01e90a120e8fd0d
folcogiorgetti <71450866+folcogiorgetti@users.noreply.github.com>
2022-05-20 12:52:13 +0200

    dynamic selects

--- app/server.js
+++ app/server.js
@@ -24,16 +24,6 @@ let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 let databaseName = "teachers";
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-
-/*MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-  if (err) throw err;
-  let db = client.db(databaseName);
-  db.createCollection("teachers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-  });
-});*/
-
 app.post('/get-insegnante', function (req, res) {
   let response = {};
 
@@ -58,21 +48,26 @@ app.post('/get-insegnante', function (req, res) {
   });
 });
 
-/*app.post('/get-select', function (req, res) {
+app.post('/get-select', function (req, res) {
   let response = {};
 
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
-    //let città = req.body.città;
-    //let materia = req.body.materia;
+    let città = req.body.città;
+    let materia = req.body.materia;
     //console.log(città);
-    let myquery = { nome: "Folco" };
+    
+    db.collection("teachers").find({}, { projection :{città:1,materie:1,_id:0}}).toArray(function(err, result)  {
+      if (err) throw err;
+      response = result;
+      console.log(response);
+      client.close();
 
-    const test = db.collection("teachers").distinct("cognome", myquery);
-    console.log(test);
+      // Send response
+      res.send(response ? response : {});
   });
-});*/
-
+});
+});
 
