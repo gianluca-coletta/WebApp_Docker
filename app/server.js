5fc5e5ff406ab80295d65d2063b578c7d201fb7a
Gianluca <coligian9@gmail.com>
2022-05-21 16:07:48 +0200

    little change

--- app/server.js
+++ app/server.js
@@ -2,24 +2,24 @@ var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 
 var express = require('express')
-    , http = require('http')
-    , path = require('path')
-    , bodyParser = require('body-parser')
-    , app = express();
+  , http = require('http')
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
 
 // Rendering index.html
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, function () {
-    console.log("app listening on port 3000!");
+  console.log("app listening on port 3000!");
 });
 
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
@@ -48,18 +48,18 @@ MongoClient.connect(mongoUrlDocker, function (err, db) {
         db.close();
       });
       var myobj = [
-        {nome: 'Folco', cognome: 'Giorgetti', materie: ['Matematica', 'Fisica','Inglese'], città: 'Perugia', telefono: '3569638798'},
-        {nome: 'Gianluca', cognome: 'Coletta', materie: ['Matematica', 'Fisica'], città: 'Perugia', telefono: '3403326423'},
-        {nome: 'Leonardo', cognome: 'Lorenzi', materie: ['Chimica'], città: 'Perugia', telefono: '3324567891'},
-        {nome: 'Riccardo', cognome: 'Tusino', materie: ['Latino', 'Inglese'], città: 'Bari', telefono: '3329781204'},
-        {nome: 'Pietro', cognome: 'Farsi ', materie: ['Matematica', 'Fisica','Inglese', 'Latino'], città: 'Milano', telefono: '3339012343'},
-        {nome: 'Paolo', cognome: 'Rossi', materie: ['Matematica', 'Informatica'], città: 'Milano', telefono: '3315622289'},
-        {nome: 'Michele', cognome: 'Verdi', materie: ['Chimica', 'Biologia'], città: 'Bari', telefono: '3334568975'},
-        {nome: 'Gianni', cognome: 'Bianchi', materie: ['Fisica', 'Matematica', 'Inglese', 'Informatica'], città: 'Terni', telefono: '3405695872'},
-        {nome: 'Benedetta', cognome: 'Mancinelli', materie: ['Economia', 'Matematica'], città: 'Terni', telefono: '3506891235'},
-        {nome: 'Paola', cognome: 'Verdi', materie: ['Fisica'], città: 'Terni', telefono: '3405623215'},
-        {nome: 'Francesco', cognome: 'Pirelli', materie: ['Fisica', 'Informatica'], città: 'Bari', telefono: '3403692581'},
-        {nome: 'Gianfranco', cognome: 'Pirillo', materie: ['Inglese'], città: 'Terni', telefono: '3506981237'}
+        { nome: 'Folco', cognome: 'Giorgetti', materie: ['Matematica', 'Fisica', 'Inglese'], città: 'Perugia', telefono: '3569638798' },
+        { nome: 'Gianluca', cognome: 'Coletta', materie: ['Matematica', 'Fisica'], città: 'Perugia', telefono: '3403326423' },
+        { nome: 'Leonardo', cognome: 'Lorenzi', materie: ['Chimica'], città: 'Perugia', telefono: '3324567891' },
+        { nome: 'Riccardo', cognome: 'Tusino', materie: ['Latino', 'Inglese'], città: 'Bari', telefono: '3329781204' },
+        { nome: 'Pietro', cognome: 'Farsi ', materie: ['Matematica', 'Fisica', 'Inglese', 'Latino'], città: 'Milano', telefono: '3339012343' },
+        { nome: 'Paolo', cognome: 'Rossi', materie: ['Matematica', 'Informatica'], città: 'Milano', telefono: '3315622289' },
+        { nome: 'Michele', cognome: 'Verdi', materie: ['Chimica', 'Biologia'], città: 'Bari', telefono: '3334568975' },
+        { nome: 'Gianni', cognome: 'Bianchi', materie: ['Fisica', 'Matematica', 'Inglese', 'Informatica'], città: 'Terni', telefono: '3405695872' },
+        { nome: 'Benedetta', cognome: 'Mancinelli', materie: ['Economia', 'Matematica'], città: 'Terni', telefono: '3506891235' },
+        { nome: 'Paola', cognome: 'Verdi', materie: ['Fisica'], città: 'Terni', telefono: '3405623215' },
+        { nome: 'Francesco', cognome: 'Pirelli', materie: ['Fisica', 'Informatica'], città: 'Bari', telefono: '3403692581' },
+        { nome: 'Gianfranco', cognome: 'Pirillo', materie: ['Inglese'], città: 'Terni', telefono: '3506981237' }
       ];
       dbo.collection("teachers").insertMany(myobj, function (err, res) {
         if (err) throw err;
@@ -79,9 +79,9 @@ app.post('/get-insegnante', function (req, res) {
     let db = client.db(databaseName);
     let città = req.body.città;
     let materia = req.body.materia;
-    let myquery = {$and: [{città: città}, {materie: materia}]};
+    let myquery = { $and: [{ città: città }, { materie: materia }] };
 
-    db.collection("teachers").find(myquery).toArray(function(err, result)  {
+    db.collection("teachers").find(myquery).toArray(function (err, result) {
       if (err) throw err;
       response = result;
       client.close();
@@ -103,15 +103,15 @@ app.post('/get-select', function (req, res) {
     let db = client.db(databaseName);
     let città = req.body.città;
     let materia = req.body.materia;
-    
-    db.collection("teachers").find({}, { projection :{città:1,materie:1,_id:0}}).toArray(function(err, result)  {
+
+    db.collection("teachers").find({}, { projection: { città: 1, materie: 1, _id: 0 } }).toArray(function (err, result) {
       if (err) throw err;
       response = result;
       client.close();
 
       // Send response
       res.send(response ? response : {});
+    });
   });
-});
 });
 
